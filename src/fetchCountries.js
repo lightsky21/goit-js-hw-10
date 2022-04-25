0e082cf2074ae8450735523b3fafca86c51ab8e3
Mariia Telehonenko <telegonenko1@ukr.net>
2022-04-26 02:12:56 +0300

    add markup

---
+++ src/fetchCountries.js
@@ -0,0 +1,12 @@
+export { fetchCountries };
+
+const fetchCountries = function(name) {
+    const url = `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`;
+   return fetch(url).then((response) => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+     return response.json()
+    })
+};
+
--- src/index.js
+++ src/index.js
@@ -1,3 +1,71 @@
 import './css/styles.css';
 
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { fetchCountries } from './fetchCountries'
+
+const inputSearchEl = document.querySelector('input[id="search-box"]');
+const countryListEl = document.querySelector('.country-list');
+const countryInfoEl = document.querySelector('.country-info');
+
+
 const DEBOUNCE_DELAY = 300;
+
+
+inputSearchEl.addEventListener('input', debounce(oninputSearchElInput, DEBOUNCE_DELAY));
+
+
+function oninputSearchElInput(e) {
+    clear()
+
+    let inputSearchElValue = inputSearchEl.value.trim();
+
+
+    fetchCountries(inputSearchElValue).then(countries => {
+        
+     if (inputSearchElValue === '') {
+         clear(); 
+    } else if((countries.length > 1) & (countries.length <= 10)) {
+         addMarkupList(countries)  
+    } else if (countries.length === 1) {
+         addMarkupCountry(countries);  
+     } else { 
+         Notify.info("Too many matches found. Please enter a more specific name.");
+     }
+
+    }).catch((error) => Notify.failure("Oops, there is no country with that name"))
+    
+}
+ 
+
+
+
+function clear() {
+countryListEl.innerHTML = '';
+countryInfoEl.innerHTML = '';
+};
+
+
+function addMarkupList(countries) {
+    countryInfoEl.innerHTML = '';
+    
+    const markupList = countries.map((country) => {
+        return `<li class="country-list-item">
+                <img src="${country.flags.svg}" alt="flag" class="flag-img">
+                ${country.name.official}
+               </li>`
+            }).join("");
+    countryListEl.innerHTML = markupList;
+}
+            
+
+function addMarkupCountry(countries) {
+    countryListEl.innerHTML = '';
+      const languages = Object.values(countries[0].languages).join(', ');
+
+      const markupInfo = `<span class="country-info-name"><img src="${countries[0].flags.svg}" alt="flag" class="flag-img">${countries[0].name.official}</span><ul class="country-info-list">
+      <li class="country-info-item">Capital: ${countries[0].capital}</li>
+      <li class="country-info-item">Population: ${countries[0].population}</li>
+      <li class="country-info-item">Languages: ${languages}</li>
+      </ul>`;
+        countryInfoEl.innerHTML = markupInfo;}
